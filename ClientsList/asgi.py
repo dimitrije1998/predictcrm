ef49a5f5fbd27ce3ba74b58bb143d87533d1ff9b
dimitrije1998 <dimitrijek1998@gmail.com>
2025-08-10 11:50:38 -0700

    Sitne izmene u ClientsList/asgi.py

--- ClientsList/asgi.py
+++ ClientsList/asgi.py
@@ -1,11 +1,22 @@
+"""
+ASGI config for ClientsList project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
 import os
+
 from dotenv import load_dotenv
-from django.core.asgi import get_asgi_application
 
 load_dotenv()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClientsList.settings')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-api_key = os.getenv("OPENAI_API_KEY")
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClientsList.settings')
 
 application = get_asgi_application()
