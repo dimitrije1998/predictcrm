e05c0346894e829b03ae8300deeba2f79d398b71
dimitrije1998 <dimitrijek1998@gmail.com>
2025-08-10 13:37:11 -0700

    Add custom domains to ALLOWED_HOSTS

--- ClientsList/settings.py
+++ ClientsList/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*1&%93irv@)%2h$znp@escj^$gx^z*+j3&1t=)puz@#a6db7(s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.predict-crm.com', 'predictcrm.com']
 
 
 # Application definition
